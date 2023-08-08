377e4fc3b059ab38eb13434d7ca42889763700d0
wen <635210311@qq.com>
2023-08-08 20:28:06 +0800

    测试

---
+++ citywalk-mp-backend-main/src/test/java/com/aspirants/citywalkgame/CitywalkGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirants.citywalkgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitywalkGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
