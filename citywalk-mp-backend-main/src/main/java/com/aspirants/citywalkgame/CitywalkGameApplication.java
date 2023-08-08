b66cad958debe84045fa1cd0063a673edc9b0ebf
wen <635210311@qq.com>
2023-08-08 20:41:33 +0800

    测试

--- citywalk-mp-backend-main/src/main/java/com/aspirants/citywalkgame/CitywalkGameApplication.java
+++ citywalk-mp-backend-main/src/main/java/com/aspirants/citywalkgame/CitywalkGameApplication.java
@@ -8,7 +8,7 @@ public class CitywalkGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitywalkGameApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello wold");
 	}
 
 }
