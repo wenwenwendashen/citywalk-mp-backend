5ed514581f73e1dba7c9158d9564f7aacc45cd6c
Li Jing <3202146864@qq.com>
2023-08-08 15:27:38 +0800

    first commit

---
+++ src/main/java/com/aspirants/citywalkgame/CitywalkGameApplication.java
@@ -0,0 +1,13 @@
+package com.aspirants.citywalkgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitywalkGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitywalkGameApplication.class, args);
+	}
+
+}
